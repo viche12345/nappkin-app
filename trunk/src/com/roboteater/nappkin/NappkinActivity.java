6c15d07ce1c13bc0750df1ae44ecab40480508d4
viche12345@gmail.com <viche12345@gmail.com@8bbfb9bb-0031-9c4a-e3c9-19973ef045fe>
2012-03-03 08:17:04 +0000

    Added the ability to select AND deselect different bubbles and save a reference to the selected bubble. Selecting a bubble causes it to be highlighted. To deselect, click on a highlighted bubble. Also, added dragging ability to a selected bubble.

--- trunk/src/com/roboteater/nappkin/NappkinActivity.java
+++ trunk/src/com/roboteater/nappkin/NappkinActivity.java
@@ -9,13 +9,13 @@ import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 public class NappkinActivity extends Activity {
 	
 	private FrameLayout mainView;
 	private ArrayList<Bubble> listOfBubbles = new ArrayList<Bubble>();
 	private GestureDetector gestureDetector;
+	private Bubble selectedBubble;
 	
     /** Called when the activity is first created. */
     @Override
@@ -36,32 +36,51 @@ public class NappkinActivity extends Activity {
     }
     
     class MyGestureDetector extends SimpleOnGestureListener {
+    	
+    	boolean draggingBubble;
+    	
 		@Override
 		public boolean onDown(MotionEvent e) {
+			if (selectedBubble != null && selectedBubble.contains((int)e.getX(), (int)e.getY())){
+				draggingBubble = true;
+			} else draggingBubble = false;
 			return true;
 		}
 
 		@Override
 		public boolean onSingleTapUp(MotionEvent e) {
+			boolean selected = false;
 			int x = (int)e.getX();
 			int y = (int)e.getY();
 			for (Bubble b : listOfBubbles) {
 				if (b.contains(x,y)) {
-					Toast.makeText(getApplicationContext(), "Clicked!", Toast.LENGTH_SHORT).show();
-					return true;
+					boolean highlighted = b.select();
+					if (highlighted) {
+						if (selectedBubble != null) selectedBubble.select();
+						selectedBubble = b;
+					}
+					else {
+						selectedBubble = null;
+					}
+					selected = true;
 				}
 			}
-			Bubble c = new Bubble(getApplicationContext(), x, y);
-			mainView.addView(c);
-			listOfBubbles.add(c);
+			if (!selected) {
+				Bubble c = new Bubble(getApplicationContext(), x, y);
+				mainView.addView(c);
+				listOfBubbles.add(c);
+			}
 			return true;
 		}
 
 		@Override
 		public boolean onScroll(MotionEvent e1, MotionEvent e2,
 				float distanceX, float distanceY) {
-			for (Bubble circ : listOfBubbles) {
-				circ.shift((int)-distanceX, (int)-distanceY);
+			if (draggingBubble) selectedBubble.shift((int)-distanceX, (int)-distanceY);
+			else {
+				for (Bubble circ : listOfBubbles) {
+					circ.shift((int)-distanceX, (int)-distanceY);
+				}
 			}
 			return true;
 		}
