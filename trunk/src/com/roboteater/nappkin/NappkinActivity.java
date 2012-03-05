dc7f5a37ba638e52479c56a4895f3f49897f854e
qasidsadiq@gmail.com <qasidsadiq@gmail.com@8bbfb9bb-0031-9c4a-e3c9-19973ef045fe>
2012-03-05 03:13:21 +0000

    Changed sendMessage "update" command to proper second thread command.

--- trunk/src/com/roboteater/nappkin/NappkinActivity.java
+++ trunk/src/com/roboteater/nappkin/NappkinActivity.java
@@ -70,8 +70,8 @@ public class NappkinActivity extends Activity {
 			
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-//				if (event.getAction()==MotionEvent.ACTION_UP) 
-//					sendMessage(null, "update");
+				if (event.getAction()==MotionEvent.ACTION_UP)
+					new Update().execute(null, "update");
 				return gestureDetector.onTouchEvent(event);
 			}
 		});
